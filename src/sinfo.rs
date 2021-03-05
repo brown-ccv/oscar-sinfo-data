579a3b159a73932067504950b5c5a7f1bef158a3
Paul Stey <paulstey@gmail>
2021-03-05 12:18:03 -0500

    un-comment the sinfo call

--- src/sinfo.rs
+++ src/sinfo.rs
@@ -3,23 +3,23 @@ use std::process::{Command, Stdio};
 
 
 pub fn cpu_status() -> Vec<String> {
-    // let process = match Command::new("/usr/local/bin/sinfo")
-    //                             .arg("--all")
-    //                             .arg("--format=%C")
-    //                             .arg("--noheader")
-    //                             .stdout(Stdio::piped())
-    //                             .spawn() {
-    //     Err(why) => panic!("Couldn't spawn sinfo: {}", why),
-    //     Ok(process) => process,
-    // };
+    let process = match Command::new("/usr/local/bin/sinfo")
+                                .arg("--all")
+                                .arg("--format=%C")
+                                .arg("--noheader")
+                                .stdout(Stdio::piped())
+                                .spawn() {
+        Err(why) => panic!("Couldn't spawn sinfo: {}", why),
+        Ok(process) => process,
+    };
 
-    // // The `stdout` field also has type `Option<ChildStdout>` so must be unwrapped.
-    // let mut s = String::new();
-    // match process.stdout.unwrap().read_to_string(&mut s) {
-    //     Err(why) => panic!("Couldn't read sinfo stdout: {}", why),
-    //     Ok(_) => print!("sinfo responded with:\n{}", s),
-    // };
-    let s = String::from("8446/5042/72/13560");
+    // The `stdout` field also has type `Option<ChildStdout>` so must be unwrapped.
+    let mut s = String::new();
+    match process.stdout.unwrap().read_to_string(&mut s) {
+        Err(why) => panic!("Couldn't read sinfo stdout: {}", why),
+        Ok(_) => print!("sinfo responded with:\n{}", s),
+    };
+    // The String `s` will be something of the form: "8446/5042/72/13560"  
 
     let cpu_nums: Vec<String> = s.split('/').map(|x| String::from(x)).collect();
 
