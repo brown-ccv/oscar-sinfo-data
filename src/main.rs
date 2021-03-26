eeba7c450525f32239c47e7516e52e1e3b46d9da
Paul Stey <paulstey@gmail.com>
2021-03-25 21:55:13 -0400

    fix merge confliect cruft

--- src/main.rs
+++ src/main.rs
@@ -8,27 +8,7 @@ mod sinfo;
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
-<<<<<<< HEAD
-    println!("Username:");
-    let mut user = String::new();
 
-    io::stdin()
-        .read_line(&mut user)
-        .expect("Failed to username");
-    
-    let user = user.trim();
-    let pass = rpassword::read_password_from_tty(Some("Password: ")).unwrap();
-    let conn_str = format!("mysql://{}:{}@pventmydbcit1.services.brown.edu", user, pass);
-    
-    println!("{}", conn_str);
-    
-    let seconds = time::Duration::from_secs(5);
-    let pool = MySqlPool::connect(&conn_str).await;
-
-    let pool = pool.unwrap();
-=======
->>>>>>> feat-use-sqlite
-    
     let seconds = time::Duration::from_secs(900);
     let pool = SqlitePool::connect("sqlite:/users/pstey/projects/oscar-sinfo-data/src/sql/oscar.db").await?; 
 
@@ -49,7 +29,3 @@ async fn main() -> Result<(), sqlx::Error> {
 
     Ok(())
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> feat-use-sqlite
