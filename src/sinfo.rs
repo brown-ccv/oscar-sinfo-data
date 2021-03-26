1fdf1a066739ba5cc5f3e150acd736e59a885f14
Paul Stey <paulstey@gmail.com>
2021-03-25 21:56:13 -0400

    fix merge conflict cruft

--- src/sinfo.rs
+++ src/sinfo.rs
@@ -3,27 +3,7 @@ use std::process::{Command, Stdio};
 
 
 pub fn cpu_status() -> Vec<String> {
-<<<<<<< HEAD
-    // let process = match Command::new("/usr/local/bin/sinfo")
-    //                             .arg("--all")
-    //                             .arg("--format=%C")
-    //                             .arg("--noheader")
-    //                             .stdout(Stdio::piped())
-    //                             .spawn() {
-    //     Err(why) => panic!("Couldn't spawn sinfo: {}", why),
-    //     Ok(process) => process,
-    // };
 
-    // // The `stdout` field also has type `Option<ChildStdout>` so must be unwrapped.
-    // let mut s = String::new();
-    // match process.stdout.unwrap().read_to_string(&mut s) {
-    //     Err(why) => panic!("Couldn't read sinfo stdout: {}", why),
-    //     Ok(_) => print!("sinfo responded with:\n{}", s),
-    // };
-    // The String `s` will be something of the form: "8446/5042/72/13560"
-    let s = String::from("8446/5042/72/13560");
-
-=======
     let mut process = match Command::new("/usr/local/bin/sinfo")
                                     .arg("--all")
                                     .arg("--format=%C")
@@ -35,7 +15,6 @@ pub fn cpu_status() -> Vec<String> {
     };
 
     // The `stdout` field also has type `Option<ChildStdout>` so must be unwrapped.
->>>>>>> feat-use-sqlite
 
     let output = process 
         .wait_with_output()
